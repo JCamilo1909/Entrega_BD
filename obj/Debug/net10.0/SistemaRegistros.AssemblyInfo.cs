04e39ea3065be2c505966d3102958c00cd540da8
Juan Camilo G <juancamilo190901@gmail.com>
2026-06-03 10:40:35 -0500

    Se crea con el fin de ejecutarlo en docker

--- obj/Debug/net10.0/SistemaRegistros.AssemblyInfo.cs
+++ obj/Debug/net10.0/SistemaRegistros.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaRegistros")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0cd04896403c266913d0e5128cf382dc11d9290")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c99153e047aaa4d5c923ee15547dc8c3d0e6c6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaRegistros")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaRegistros")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
